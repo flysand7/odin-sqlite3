677f605afbd565ce9dd52e43947040df8b72ca16
dozn <16659513+dozn@users.noreply.github.com>
2025-04-28 12:10:55 -0700

    Fix sqlite3.obj Path While Building on Windows

--- build-cl.bat
+++ build-cl.bat
@@ -1,4 +1,4 @@
 @echo off
 cl /nologo /c amalgamation/sqlite3.c -Fo:bindings/bin/sqlite3.obj
-lib /nologo bin/sqlite3.obj /out:"bindings/bin/sqlite3.lib"
+lib /nologo bindings/bin/sqlite3.obj /out:"bindings/bin/sqlite3.lib"
 echo bindings/sqlite3.lib compiled!
